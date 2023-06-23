5735be470911e043166e87ef8ed0e115b7b90282
Harshita <chandaliyaharshita222@gmail.com>
2023-06-23 19:14:02 +0530

    Basic concept on class and object

---
+++ Basic_Concepts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Basic_Concepts
+{
+    public class Program
+    {
+        string brand = "Mahindra";
+        string color = "Red";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a program with object and class concept with example");
+            Program myObj = new Program();
+            Console.WriteLine("Car brand = " + myObj.brand);
+            Console.WriteLine("Car color = " + myObj.color);
+        }
+    }
+}
