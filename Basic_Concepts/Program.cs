2fbc1e93ceca5af9801299a4c62d97cd8ddef7f6
Harshita <chandaliyaharshita222@gmail.com>
2023-06-23 21:27:56 +0530

    Types of methods in CSharp

--- Basic_Concepts/Program.cs
+++ Basic_Concepts/Program.cs
@@ -3,21 +3,37 @@ using System.Threading.Channels;
 
 namespace Basic_Concepts
 {
+    //Creating Static Class
+    public static class StaticClass
+    {
+        //Creating Static Method
+        public static void ReturnValue()
+        {
+            int a = 10, b = 20;
+            int sum = a + b;
+            Console.WriteLine("Sum is = " + sum);
+        }
+    }
+
+    //Creating Abstract Class
+    abstract class test1
+    {
+        //Creating Abstract Method
+        public abstract int mul(int i, int j);
+    }
+    class test2 : test1
+    {
+        //Overriding Abstract Method
+        public override int mul(int i, int j)
+        {
+            return i * j;
+        }
+    }
     public class Program
     {
-        // instance variable
-        int a = 10;
-
-        // static variable
-        static int b = 20;
-
-        // constant variable
-        const float max = 50;
-
-        // Creating Method for local variable
+        // Creating Instance Method
         public void StudentAge()
         {
-            // local variable age
             int age = 0;
 
             age = age + 18;
@@ -25,13 +41,16 @@ namespace Basic_Concepts
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Create a program to demonstrate types of variables present in c#");
+            Console.WriteLine("Create a program to demonstrate types of methods in c#");
             Program myObj = new Program();
-            Console.Write("\nThe value of local variable, ");
+            Console.WriteLine("\nOutput of Instance method");
             myObj.StudentAge();
-            Console.WriteLine("The value of instance variable, a is = " + myObj.a);
-            Console.WriteLine("The value of static variable, b is = " + Program.b);
-            Console.WriteLine("The value of constant variable, max is = " + Program.max);
+            Console.WriteLine("\nOutput of Static method");
+            StaticClass.ReturnValue();
+            Console.WriteLine("\nOutput of Abstract method");
+            test1 ob1 = new test2();
+            int b = ob1.mul(4, 2);
+            Console.WriteLine("4 * 2 = " + b);
         }
     }
 }
