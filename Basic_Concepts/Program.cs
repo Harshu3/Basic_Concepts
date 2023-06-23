e770430f519b7a754278957a1685db6398ba34cc
Harshita <chandaliyaharshita222@gmail.com>
2023-06-23 20:01:33 +0530

    Types of variables in CSharp

--- Basic_Concepts/Program.cs
+++ Basic_Concepts/Program.cs
@@ -5,15 +5,33 @@ namespace Basic_Concepts
 {
     public class Program
     {
-        string brand = "Mahindra";
-        string color = "Red";
+        // instance variable
+        int a = 10;
 
+        // static variable
+        static int b = 20;
+
+        // constant variable
+        const float max = 50;
+
+        // Creating Method for local variable
+        public void StudentAge()
+        {
+            // local variable age
+            int age = 0;
+
+            age = age + 18;
+            Console.WriteLine("Student age is : " + age);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Create a program with object and class concept with example");
+            Console.WriteLine("Create a program to demonstrate types of variables present in c#");
             Program myObj = new Program();
-            Console.WriteLine("Car brand = " + myObj.brand);
-            Console.WriteLine("Car color = " + myObj.color);
+            Console.Write("\nThe value of local variable, ");
+            myObj.StudentAge();
+            Console.WriteLine("The value of instance variable, a is = " + myObj.a);
+            Console.WriteLine("The value of static variable, b is = " + Program.b);
+            Console.WriteLine("The value of constant variable, max is = " + Program.max);
         }
     }
 }
